56291e0e06b135a9f9084cee30d40bcc92c6cf7f
KaiyangLi1 <42019678+KaiyangLi1@users.noreply.github.com>
2019-02-09 20:39:50 +1100

    upload

---
+++ scroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroller : MonoBehaviour {
+
+    public Renderer rend;
+    float offset;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset = Time.time * speed;
+        rend.material.mainTextureOffset = new Vector2(0, offset);
+		
+	}
+}
